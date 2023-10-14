c29f492d6683308b958a531a6a99e086b49ae6ee
kubBytes <kubBytes@gmail.com>
2023-10-14 20:01:15 +0900

    add-endpoint-JWT & change some logics

--- JWT-auth-logic.js
+++ JWT-auth-logic.js
@@ -1,20 +1,24 @@
 import jwt from 'jsonwebtoken';
 
+//jwt.sign, jwt.varifyは非同期処理
+
 //トークン生成関数
 export function generateJWT (user, secretKey) {
-    jwt.sign(user, secretKey, (err, token) => {
-        if(err){
-            return {token: null, error: "トークン生成に失敗しました。"}
-        }else{
-            return {token: token, error: null};
-        }
+    return new Promise((resolve, reject) => {
+        jwt.sign(user, secretKey, (err, token) => {
+            if(err){
+                reject({token: null, error: "トークン生成に失敗しました。"});
+            }else{
+                resolve({token: token, error: null});
+            }
+        });
     });
 }
 
-//認可ミドルウェア
-export function authorizeJWT (req, secretKey, next) {
+//トークンの検証
+export function authorizeJWT (req, secretKey) {
     //ヘッダーからトークンを取得
-    const token = req.header(Authorization);
+    const token = req.header('Authorization');
 
     //トークンが存在しない場合は401 Unauthorizedを返す
     if(!token){
@@ -22,12 +26,13 @@ export function authorizeJWT (req, secretKey, next) {
     }
 
     //トークンを検証
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if(err){
-            return {decoded: null, error: "トークンの検証に失敗しました"};
-        }
-
-        req.user = decoded.user;
-        next();
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secretKey, (err, decoded) => {
+            if(err){
+                reject({decoded: null, error: "トークンの検証に失敗しました"});
+            }else{
+                resolve({decoded: decoded.username, error: null});
+            }
+        });
     });
 }
--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import { authorize } from './auth-logic.js';
 import { generateAuthToken } from './auth-logic.js';
+import { generateJWT } from './JWT-auth-logic.js';
+import { authorizeJWT } from './JWT-auth-logic.js';
 const app = express()
 const port = 3000
 
 /* -------------------- フルスクラッチのトークンベース認可 -------------------- */
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
     //ユーザ情報
     const user = {
         id: 1,
-        username: 'test_user_at_auth-token-tutorials',
-        email: 'test_email_at_auth-token-tutorials@email.com'
+        username: 'username',
+        email: 'email'
     };
 
     //シンプルなトークンを生成
@@ -27,7 +29,44 @@ app.get('/protected-resource', authorize, (req, res) => {
 
 /* -------------------- JWTを使ったトークンベース認可 -------------------- */
 //秘密鍵
-secretKey = 'userSecretKey';
+const secretKey = 'userSecretKey';
+
+// トークン生成のエンドポイント
+app.post('/login-JWT', async (req, res) => {
+    //ユーザ情報
+    const user = {
+        id: 1,
+        username: 'username',
+        email: 'email'
+    };
+
+    try {
+        const result = await generateJWT(user, secretKey);
+        if (result.error) {
+            res.status(500).json(result);
+        } else {
+            res.status(200).json(result);
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//プロテクトされたリソースにアクセスするエンドポイント
+app.get('/protected-resource-JWT', async (req, res) => {
+
+    try {
+        const result = await authorizeJWT(req, secretKey);
+        if(result.error){
+            res.status(500).json(result.error);
+        }else{
+            res.status(200).json({decodedUsername: result.decoded, message: "プロテクトされたリソースにアクセスしました。"});
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+
+});
 
 
 app.listen(port, () => {
