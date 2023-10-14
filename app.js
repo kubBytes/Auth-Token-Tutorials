1a4fa1a334258c2bc7c1b87439fbb63966ea7b43
kubBytes <kubBytes@gmail.com>
2023-10-14 14:57:34 +0900

    add-endpoint

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const express = require('express')
+import express from 'express';
+import { authorize } from './auth-logic.js';
+import { generateAuthToken } from './auth-logic.js';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/login', (req, res) => {
+    //ユーザ情報
+    const user = {
+        id: 1,
+        username: 'test_user_at_auth-token-tutorials',
+        email: 'test_email_at_auth-token-tutorials@email.com'
+    };
+
+    //シンプルなトークンを生成
+    const authToken = generateAuthToken(user);
+
+    res.json({ authToken });
+});
+
+//プロテクトされたリソースにアクセスするエンドポイント
+app.get('/protected-resource', authorize, (req, res) => {
+    res.json({ message: "プロテクトされたリソースにアクセスしました" });
+});
 
 app.listen(port, () => {
   console.log(`${port}番ポートでアプリを実行中です。`)
--- auth-logic.js
+++ auth-logic.js
@@ -1,11 +1,5 @@
-//JSONパーサーを利用する
-append.use(bodyParser.json());
-
-//シークレットトークン
-export const secretToken = 'your-secret-token';
-
 //トークン生成関数
-export function generateAuthToken (user) {
+ export function generateAuthToken (user) {
     const userJSON = JSON.stringify(user);
     const userBase64 = Buffer.from(userJSON).toString('base64');
 
@@ -13,7 +7,7 @@ export function generateAuthToken (user) {
 }
 
 //認可ミドルウェア
-export function authorize (req, res, next) {
+ export function authorize (req, res, next) {
     //ヘッダーからトークンを取得
     const authToken = req.header('Authorization');
 
@@ -22,7 +16,7 @@ export function authorize (req, res, next) {
         return res.status(401).json({error : "トークンが提供されていません"});
     }
 
-    //トークンを検証
+    //トークンを検証, 403 Forbiddenはアクセス禁止
     const user = validateAuthToken(authToken);
     if (!user) {
         return res.status(403).json({error : "トークンの検証に失敗しました"});
@@ -34,15 +28,14 @@ export function authorize (req, res, next) {
 }
 
 //トークン検証関数
-export function validateAuthToken (authToken) {
+ function validateAuthToken (authToken) {
     //シンプルなトークンをデコード
     const authTokenWithoutBearer = authToken.replace('Bearer ', '');
-    const tokenData = Buffer.from(authTokenWithoutBearer, 'base64').ttoString('utf-8');
+    const tokenData = Buffer.from(authTokenWithoutBearer, 'base64').toString('utf-8');
 
     try {
         return JSON.parse(tokenData);
     } catch (error) {
         return null;
     }
-
 }
